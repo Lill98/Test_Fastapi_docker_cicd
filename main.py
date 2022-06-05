21975bb5a073811244da3f2bb75c30864434971d
Lill <53203795+Lill98@users.noreply.github.com>
2022-06-05 23:56:30 +0700

    Update main.py
    test function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+print("hello)
